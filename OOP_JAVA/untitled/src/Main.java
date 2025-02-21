68a7e2fadda2ea6299aff757238854bcf52d3ad9
TheGitSlender <thegitslender@gmail.com>
2025-02-21 10:29:50 +0100

    last commit this day

--- OOP_JAVA/untitled/src/Main.java
+++ OOP_JAVA/untitled/src/Main.java
@@ -1,10 +1,57 @@
 
 public class Main {
-    public static void main(String[] args) {
-        int[] table = new int[10];
-        for (int i = 0; i<10;i++){
-            table[i] = i-5;
+    static int[] table_sort(int[] table) {
+        for (int i = 0; i < table.length - 1; i++) {
+            for (int j = i + 1; j < table.length; j++) {
+                if (table[i] > table[j]) {
+                    int temp = table[i];
+                    table[i] = table[j];
+                    table[j] = temp;
+                }
+            }
         }
-        System.out.println(table[0]);
+        return table;
+    }
+    static int[][] table_sort2(int[] table) {
+        int[] negative = new int[table.length-1];
+        int neg_index = 0;
+        int[] positive = new int[table.length-1];
+        int pos_index = 0;
+        for (int i = 0; i < table.length; i++) {
+            int min_index = i;
+            for (int j = i; j < table.length; j++) {
+                if (table[min_index] > table[j]) {
+                    min_index = j;
+                }
+            }
+            int min = table[min_index];
+            table[min_index] = table[i];
+            table[i] = min;
+            if (table[i]<0) {
+                negative[neg_index] = table[i];
+                neg_index++;
+            } else {
+                positive[pos_index] = table[i];
+                pos_index++;
+            }
+        }
+        int[][] result = new int[2][];
+        result [0] = java.util.Arrays.copyOf(negative,neg_index);
+        result [1] = java.util.Arrays.copyOf(positive,pos_index);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] table = {3, -1, 4, -5, 2};
+        int[][] sortedTables = table_sort2(table);
+
+        System.out.println("Negative numbers:");
+        for (int i : sortedTables[0]) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\nPositive numbers:");
+        for (int i : sortedTables[1]) {
+            System.out.print(i + " ");
+        }
     }
 }
