209e594ab69be7869fd7cdcae71d50eb74b1bf75
TheGitSlender <thegitslender@gmail.com>
2025-03-07 21:13:11 +0000

    tp js (to redo)

--- Web_Dev/web_stat/TP_3_JS/Exercice_0/main.js
+++ Web_Dev/web_stat/TP_3_JS/Exercice_0/main.js
@@ -1,5 +1,16 @@
-function change_color(newcolor){
+// 34
+function change_color(newColor){
     var elem = document.getElementById('para');
     elem.innerHTML = "Bravo ! ";
-    elem.style.color = newcolor;
+    elem.style.color = newColor;
 }
+
+var Bout = document.getElementsByTagName('button');
+alert('Nb Button : ' + Bout.length);
+
+// 37
+var pa = document.getElementsByClassName('cls'); // pa est un tableau
+pa[0].style.color = "red";
+pa[0].style.fontSize = "25px";
+pa[0].style.fontWeight = "bold";
+pa[1].innerHTML = "Nouveau premier ";
